e7c7a655a5542ddf873066722286955fd820642f
Nir Adler <nira7008@gmail.com>
2021-10-25 09:08:50 +0300

    feature version 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          this is new from feature branch
+          this is new from feature branch version 2
         </p>
         
       </header>
